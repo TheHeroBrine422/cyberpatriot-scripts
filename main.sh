c4db9da708f09da063e23d126420c55361cf802f
Hero <caleb@calebgj.io>
2022-01-20 15:37:43 -0600

    switch to wget

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 sudo apt -y install node 
-curl -O https://raw.githubusercontent.com/TheHeroBrine422/cyberpatriot-scripts/main/main.js
+wget https://raw.githubusercontent.com/TheHeroBrine422/cyberpatriot-scripts/main/main.js
 echo "Configure options (first variables in main.js), then run `sudo node main.js`"
