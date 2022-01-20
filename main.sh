be82fdd0664356d800efb16efb8b2a6488316727
Hero <caleb@calebgj.io>
2022-01-20 17:11:38 -0600

    update shellscript

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
 sudo apt -y install nodejs
+mkdir script
+cd script
 wget https://raw.githubusercontent.com/TheHeroBrine422/cyberpatriot-scripts/main/main.js
 echo "Configure options (first variables in main.js), then run `sudo node main.js`"
