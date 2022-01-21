776d643e35f28e377f93a10a6783efbfe254d7b9
Hero <caleb@calebgj.io>
2022-01-20 19:42:10 -0600

    more memory and add lists

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 1. Run: `wget -q -O - "https://raw.githubusercontent.com/TheHeroBrine422/cyberpatriot-scripts/main/main.sh" | tr -d '\r' | /bin/bash`
 2. Set settings variables in main.sh
-3. Run: `sudo node main.js`
+3. Run: `sudo node --max-old-space-size=8192 main.js`
--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@ sudo apt -y install nodejs
 mkdir script
 cd script
 wget https://raw.githubusercontent.com/TheHeroBrine422/cyberpatriot-scripts/main/main.js
-echo "Configure options (first variables in main.js), then run `sudo node main.js`"
+echo "Configure options (first variables in main.js), then run `sudo node --max-old-space-size=8192 main.js`"
