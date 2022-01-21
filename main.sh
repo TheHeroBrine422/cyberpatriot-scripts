7459357bf496a08c276d6369656c2957bf54fdba
Hero <caleb@calebgj.io>
2022-01-20 21:27:17 -0600

    move files to py.

--- main.sh
+++ main.sh
@@ -2,4 +2,5 @@ sudo apt -y install nodejs
 mkdir script
 cd script
 wget https://raw.githubusercontent.com/TheHeroBrine422/cyberpatriot-scripts/main/main.js
+wget https://raw.githubusercontent.com/TheHeroBrine422/cyberpatriot-scripts/main/files.py
 echo "Configure options (first variables in main.js), then run `sudo node --max-old-space-size=8192 main.js`"
